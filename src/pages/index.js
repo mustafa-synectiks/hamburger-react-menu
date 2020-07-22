4c76561f0e5b5cf81528fc49f1acdbe067f5ddda
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2020-07-22 23:52:06 +0530

    Adding ref to close menu on click outside

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
 import styled from 'styled-components';
-import { FiAlignRight } from 'react-icons/fi';
-import { FiX } from 'react-icons/fi';
-import Navbar from '../components/Nav/Navbar';
-import Burger from '../components/Nav/Burger.js';
-import ColumnThree from '../components/Columns/ColumnThree.js';
-import ColContent from '../components/ColumnCenter/ColContent';
 import Dropdown from './dropdown.js';
 import RightBlock from './Right/RightBlock.js';
 import LeftBlock from './Left/LeftBlock.js';
